1b4d5bc1da214281d97ad155c18844049b498b80
raulpineda96 <rnpinedar@uqvirtual.edu.co>
2021-06-29 22:48:59 -0500

    Se organizaron los archivos json que estan relacionados con las categorias y el carrusel, ademas de cambiar favicon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,11 +10,11 @@ import { Product } from "../../app.models";
 export class HomeComponent implements OnInit {
 
   public slides = [
-    { title: 'The biggest sale', subtitle: 'Special for today', image: 'assets/images/carousel/banner1.jpg' },
-    { title: 'Summer collection', subtitle: 'New Arrivals On Sale', image: 'assets/images/carousel/banner2.jpg' },
-    { title: 'The biggest sale', subtitle: 'Special for today', image: 'assets/images/carousel/banner3.jpg' },
-    { title: 'Summer collection', subtitle: 'New Arrivals On Sale', image: 'assets/images/carousel/banner4.jpg' },
-    { title: 'The biggest sale', subtitle: 'Special for today', image: 'assets/images/carousel/banner5.jpg' }
+    { title: 'Las mejores ofertas', subtitle: 'Especial del día', image: 'assets/images/carousel/banner1.jpg' },
+    { title: 'Productos especiales', subtitle: 'Nuevos productos para ofrecerte', image: 'assets/images/carousel/banner2.jpg' },
+    { title: 'Las mejores ofertas', subtitle: 'Especial del día', image: 'assets/images/carousel/banner3.jpg' },
+    { title: 'Productos especiales', subtitle: 'Nuevos productos para ofrecerte', image: 'assets/images/carousel/banner4.jpg' },
+    { title: 'Las mejores ofertas', subtitle: 'Especial del día', image: 'assets/images/carousel/banner5.jpg' }
   ];
 
   public brands = [];
