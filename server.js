1ac50847f814679638052f446750103ec374a8c1
Hector Blandon <hlblandone@uqvirtual.edu.co>
2021-08-01 17:37:37 -0500

    Se realizan ajustes para el despliegue en heroku

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 4000;
-app.use(express.static(__dirname + '/dist'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});     
-app.listen(port, () => console.log(`listening on port:${port}`));
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const port = process.env.PORT || 4000;
+// app.use(express.static(__dirname + '/dist'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });     
+// app.listen(port, () => console.log(`listening on port:${port}`));
+
+/**
+ * 
+ */
+
+ const path = require('path');
+ const express = require('express');
+ const app = express();
+ 
+ // Serve static files
+ app.use(express.static(__dirname + '/dist/primates'));
+ 
+ // Send all requests to index.html
+ app.get('/*', function(req, res) {
+   res.sendFile(path.join(__dirname + '/dist/primates/index.html'));
+ });
+ 
+ // default Heroku port
+ app.listen(process.env.PORT || 5000);
+
