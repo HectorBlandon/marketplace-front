cfc503b6db5213600a4224b8bf0764d66e70caab
HectorBlandon <74831329+HectorBlandon@users.noreply.github.com>
2021-08-01 20:23:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@
  app.use(express.static(__dirname + '/dist/primates'));
  
  // Send all requests to index.html
- app.get('/*', function(req, res) {
+ app.get('*', function(req, res) {
    res.sendFile(path.join(__dirname + '/dist/primates/index.html'));
  });
  
