b3dd7fbbd68b83873758b03514006e1c7a64aaeb
Hector Blandon <hlblandone@uqvirtual.edu.co>
2021-08-02 12:33:15 -0500

    ajustes

--- src/server.js
+++ src/server.js
@@ -17,11 +17,11 @@
  const app = express();
  
  // Serve static files
- app.use(express.static(__dirname + '/dist/primates'));
+ app.use(express.static(__dirname + 'dist/browser'));
  
  // Send all requests to index.html
- app.get('*', function(req, res) {
-   res.sendFile(path.join(__dirname + '/dist/primates/index.html'));
+ app.get('/', function(req, res) {
+   res.sendFile(path.join(__dirname + 'dist/browser/index.html'));
  });
  
  // default Heroku port
